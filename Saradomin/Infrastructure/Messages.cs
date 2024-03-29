054555afcb33b42766b32928f490a8167e01e676
vddCore <vddcore@outlook.com>
2024-03-29 07:55:55 +0100

    fix scrollbars and whatever broke after rebase

--- Saradomin/Infrastructure/Messages.cs
+++ Saradomin/Infrastructure/Messages.cs
@@ -5,4 +5,6 @@ namespace Saradomin.Infrastructure
     public record MainViewLoadedMessage : Message;
     public record NotificationBoxStateChangedMessage(bool WasOpened) : Message;
     public record SettingsModifiedMessage(string SettingName) : Message;
+    public record ClientClosedMessage : Message;
+    public record ClientLaunchRequestedMessage : Message;
 }
